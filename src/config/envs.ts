6097e7853f29e06563fdc6c092bf0ce741fdf719
Rubén FGR <rubenfranciscogr@outlook.com>
2024-08-07 09:13:59 +0200

    add envs config with joi validation

---
+++ src/config/envs.ts
@@ -0,0 +1,31 @@
+import 'dotenv/config';
+
+import * as joi from 'joi';
+
+interface EnvVars {
+  APP_PORT: number;
+  APP_URL: string;
+}
+
+const envsSchema = joi.object({
+  APP_PORT: joi.number().required(),
+  APP_URL: joi.string().required(),
+})
+.unknown(true);
+
+const { error, value } = envsSchema.validate({ 
+  ...process.env,
+});
+
+
+if ( error ) {
+  throw new Error(`Config validation error: ${ error.message }`);
+}
+
+const envVars:EnvVars = value;
+
+
+export const envs = {
+  appPort: envVars.APP_PORT,
+  appUrl: envVars.APP_URL,
+};
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { envs } from './config/envs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.setGlobalPrefix('api');
+
+  await app.listen(envs.appPort, () => {
+    console.log(`Server running on ${envs.appUrl}:${envs.appPort}`);
+  });
 }
 bootstrap();
